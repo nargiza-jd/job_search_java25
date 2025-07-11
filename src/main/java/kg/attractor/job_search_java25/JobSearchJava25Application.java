563ef9de5908a3a960ea5011e11165b721a53567
nargiza-jd <supabaevanargiza@gmail.com>
2025-07-11 10:01:19 +0600

    Initial commit

---
+++ src/main/java/kg/attractor/job_search_java25/JobSearchJava25Application.java
@@ -0,0 +1,13 @@
+package kg.attractor.job_search_java25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSearchJava25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSearchJava25Application.class, args);
+	}
+
+}
---
+++ src/test/java/kg/attractor/job_search_java25/JobSearchJava25ApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.attractor.job_search_java25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSearchJava25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
