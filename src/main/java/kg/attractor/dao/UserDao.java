2e72b43a032b4f3b41c1c837ccdfbb8311ba62d3
nargiza-jd <supabaevanargiza@gmail.com>
2025-07-19 15:39:19 +0600

    Updated the UserDao class

--- src/main/java/kg/attractor/dao/UserDao.java
+++ src/main/java/kg/attractor/dao/UserDao.java
@@ -1,12 +1,124 @@
 package kg.attractor.dao;
 
 import kg.attractor.job_search_java25.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Component;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
-public interface UserDao {
-    List<User> findByName(String name);
-    List<User> findByPhoneNumber(String phone);
-    Optional<User> findByEmail(String email);
+@Component
+@RequiredArgsConstructor
+public class UserDao {
+    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private final KeyHolder keyHolder = new GeneratedKeyHolder();
+
+    public List<User> getAllUsers() {
+        String sql = "SELECT * FROM users";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            User user = new User();
+            user.setId(rs.getInt("id"));
+            user.setName(rs.getString("name"));
+            user.setSurname(rs.getString("surname"));
+            user.setAge(rs.getInt("age"));
+            user.setEmail(rs.getString("email"));
+            user.setPassword(rs.getString("password"));
+            user.setPhoneNumber(rs.getString("phone_number"));
+            user.setAvatar(rs.getString("avatar"));
+            user.setAccountType(rs.getString("account_type"));
+            return user;
+        });
+    }
+
+    public Optional<User> getUserById(int id) {
+        String sql = "SELECT * FROM users WHERE id = ?";
+        return Optional.ofNullable(
+                DataAccessUtils.singleResult(
+                        jdbcTemplate.query(sql, (rs, rowNum) -> {
+                            User user = new User();
+                            user.setId(rs.getInt("id"));
+                            user.setName(rs.getString("name"));
+                            user.setSurname(rs.getString("surname"));
+                            user.setAge(rs.getInt("age"));
+                            user.setEmail(rs.getString("email"));
+                            user.setPassword(rs.getString("password"));
+                            user.setPhoneNumber(rs.getString("phone_number"));
+                            user.setAvatar(rs.getString("avatar"));
+                            user.setAccountType(rs.getString("account_type"));
+                            return user;
+                        }, id)
+                )
+        );
+    }
+
+    public int addUser(User user) {
+        String sql = "INSERT INTO users (name, surname, age, email, password, phone_number, avatar, account_type) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+        jdbcTemplate.update(con -> {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, user.getName());
+            ps.setString(2, user.getSurname());
+            ps.setInt(3, user.getAge());
+            ps.setString(4, user.getEmail());
+            ps.setString(5, user.getPassword());
+            ps.setString(6, user.getPhoneNumber());
+            ps.setString(7, user.getAvatar());
+            ps.setString(8, user.getAccountType());
+            return ps;
+        }, keyHolder);
+
+        return Objects.requireNonNull(keyHolder.getKey()).intValue();
+    }
+
+    public int updateUser(int id, User user) {
+        String sql = "UPDATE users SET name = ?, surname = ?, age = ?, email = ?, password = ?, phone_number = ?, avatar = ?, account_type = ? WHERE id = ?";
+        return jdbcTemplate.update(sql,
+                user.getName(),
+                user.getSurname(),
+                user.getAge(),
+                user.getEmail(),
+                user.getPassword(),
+                user.getPhoneNumber(),
+                user.getAvatar(),
+                user.getAccountType(),
+                id
+        );
+    }
+
+    public int deleteUser(int id) {
+        String sql = "DELETE FROM users WHERE id = ?";
+        return jdbcTemplate.update(sql, id);
+    }
+
+    public List<User> searchApplicants(String query) {
+        String sql = "SELECT * FROM users WHERE account_type = 'APPLICANT' AND (LOWER(name) LIKE :q OR LOWER(email) LIKE :q)";
+        return namedParameterJdbcTemplate.query(
+                sql,
+                new MapSqlParameterSource("q", "%" + query.toLowerCase() + "%"),
+                (rs, rowNum) -> {
+                    User user = new User();
+                    user.setId(rs.getInt("id"));
+                    user.setName(rs.getString("name"));
+                    user.setSurname(rs.getString("surname"));
+                    user.setAge(rs.getInt("age"));
+                    user.setEmail(rs.getString("email"));
+                    user.setPassword(rs.getString("password"));
+                    user.setPhoneNumber(rs.getString("phone_number"));
+                    user.setAvatar(rs.getString("avatar"));
+                    user.setAccountType(rs.getString("account_type"));
+                    return user;
+                }
+        );
+    }
 }
