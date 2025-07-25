799c3dccd0a92a165a1042e99c33c8f600ac3c7a
nargiza-jd <supabaevanargiza@gmail.com>
2025-07-25 14:12:52 +0600

    Added validation to the MessageCreateDto class

--- src/main/java/kg/attractor/job_search_java25/dto/MessageCreateDto.java
+++ src/main/java/kg/attractor/job_search_java25/dto/MessageCreateDto.java
@@ -1,10 +1,20 @@
 package kg.attractor.job_search_java25.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class MessageCreateDto {
+
+    @NotBlank(message = "Content must not be blank")
+    @Size(max = 1000, message = "Content must be at most 1000 characters")
     private String content;
+
+    @Min(value = 1, message = "Sender ID must be a positive number")
     private int senderId;
+
+    @Min(value = 1, message = "Receiver ID must be a positive number")
     private int receiverId;
 }
