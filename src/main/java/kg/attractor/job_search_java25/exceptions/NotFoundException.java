2614d64e09d28fa292f1945ca4f1b3e12f1e79e5
nargiza-jd <supabaevanargiza@gmail.com>
2025-07-26 01:51:52 +0600

    There was some revision regarding exceptions

--- src/main/java/kg/attractor/job_search_java25/exceptions/NotFoundException.java
+++ src/main/java/kg/attractor/job_search_java25/exceptions/NotFoundException.java
@@ -1,12 +1,7 @@
 package kg.attractor.job_search_java25.exceptions;
 
 public class NotFoundException extends RuntimeException {
-
   public NotFoundException(String message) {
     super(message);
-  }
-
-  public NotFoundException(String message, Throwable cause) {
-    super(message, cause);
   }
 }
--- src/main/java/kg/attractor/job_search_java25/exceptions/ResumeNotFoundException.java
+++ src/main/java/kg/attractor/job_search_java25/exceptions/ResumeNotFoundException.java
@@ -1,11 +1,7 @@
 package kg.attractor.job_search_java25.exceptions;
 
 public class ResumeNotFoundException extends NotFoundException {
-  public ResumeNotFoundException() {
-    super("Resume");
-  }
-
-  public ResumeNotFoundException(String message) {
-    super(message);
+  public ResumeNotFoundException(int id) {
+    super("Резюме с ID " + id + " не найдено");
   }
 }
