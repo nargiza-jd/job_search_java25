bc061305572580897031db4e3a53d00b2267cdf2
nargiza-jd <supabaevanargiza@gmail.com>
2025-07-25 14:46:15 +0600

    Added validation to the UserRegistrationDto class

--- src/main/java/kg/attractor/job_search_java25/dto/UserRegistrationDto.java
+++ src/main/java/kg/attractor/job_search_java25/dto/UserRegistrationDto.java
@@ -1,11 +1,29 @@
 package kg.attractor.job_search_java25.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UserRegistrationDto {
+
+    @NotBlank(message = "Email must not be blank")
+    @Email(message = "Email should be valid")
     private String email;
+
+    @NotBlank(message = "Password must not be blank")
+    @Size(min = 4, max = 24, message = "Password length must be between 4 and 24 characters")
+    @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).+$",
+            message = "Password must contain at least one uppercase letter, one lowercase letter, and one digit")
     private String password;
+
+    @NotBlank(message = "Name must not be blank")
+    @Size(max = 50, message = "Name must be at most 50 characters")
     private String name;
+
+    @NotBlank(message = "Account type must not be blank")
+    @Pattern(regexp = "^(applicant|employer)$", message = "Account type must be either 'applicant' or 'employer'")
     private String accountType;
 }
